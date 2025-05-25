77e9cc38ba6680397053e03a3498f04993e9d118
naumenkoks <Naumenko.ks00@gmail.com>
2025-05-25 21:09:54 +0200

    Created new project

---
+++ APIs.py
@@ -0,0 +1,7 @@
+def get_gas_price():
+    # TODO: Get price from API
+    return 8
+
+def get_power_price():
+    # TODO: Get price from API
+    return 1.2
---
+++ main.py
@@ -0,0 +1,23 @@
+import calculator
+
+
+if __name__ == '__main__':
+    calc = calculator.Calculator(mileage=15,years=3)
+    calc.add_car(
+        calculator.Car(name="Toyota Corolla",
+                       price=120000,fuel_economy=7,
+                       service_cost=1200,insurance_cost=2500)
+    )
+    calc.add_car(
+        calculator.ElectricCar(name="Tesla Model 3",price=
+                       200000, insurance_cost=5500, power_consumption=150)
+    )
+    calc.add_car(
+        calculator.Car(name="Range Rover",
+                       price=650000, fuel_economy=3,
+                       service_cost=3000, insurance_cost=7000)
+    )
+
+
+    calc.print_cars()
+
